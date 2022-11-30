4d019df5df99fe8609cc892fd930248e8c4918bd
Albert <albert.obychenie@moe.ru>
2022-11-30 21:03:56 +0500

    Завершил 3 ДЗ

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a / 10000) == (a % 10))
+{
+    a = (a / 10) % 1000;
+    if ((a / 100) == (a % 10))
+    {
+        Console.WriteLine($"Полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не полиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Не полиндром");
+}
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double dist = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)) + (Math.Pow((z2 - z1), 2)));
+Console.WriteLine($"Расстояние между двумя точками {Math.Round(dist, 2)}");
